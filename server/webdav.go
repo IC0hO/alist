1330c2a5375907ec1d033fa44f84ed600cb6c159
IC0hO <43652659+IC0hO@users.noreply.github.com>
2024-05-01 15:02:13 +0800

    Update webdav.go

--- server/webdav.go
+++ server/webdav.go
@@ -2,16 +2,12 @@ package server
 
 import (
 	"context"
-	"crypto/subtle"
 	"net/http"
 	"path"
-	"strings"
 
 	"github.com/alist-org/alist/v3/internal/conf"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/op"
-	"github.com/alist-org/alist/v3/internal/setting"
-	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/alist-org/alist/v3/server/webdav"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -48,72 +44,7 @@ func ServeWebDAV(c *gin.Context) {
 
 func WebDAVAuth(c *gin.Context) {
 	guest, _ := op.GetGuest()
-	username, password, ok := c.Request.BasicAuth()
-	if username == "" && password == "" {
-		c.Set("user", guest)
-		c.Next()
-		return
-	}
-	if !ok {
-		bt := c.GetHeader("Authorization")
-		log.Debugf("[webdav auth] token: %s", bt)
-		if strings.HasPrefix(bt, "Bearer") {
-			bt = strings.TrimPrefix(bt, "Bearer ")
-			token := setting.GetStr(conf.Token)
-			if token != "" && subtle.ConstantTimeCompare([]byte(bt), []byte(token)) == 1 {
-				admin, err := op.GetAdmin()
-				if err != nil {
-					log.Errorf("[webdav auth] failed get admin user: %+v", err)
-					c.Status(http.StatusInternalServerError)
-					c.Abort()
-					return
-				}
-				c.Set("user", admin)
-				c.Next()
-				return
-			}
-		}
-		if c.Request.Method == "OPTIONS" {
-			c.Set("user", guest)
-			c.Next()
-			return
-		}
-		c.Writer.Header()["WWW-Authenticate"] = []string{`Basic realm="alist"`}
-		c.Status(http.StatusUnauthorized)
-		c.Abort()
-		return
-	}
-	user, err := op.GetUserByName(username)
-	if err != nil || user.ValidateRawPassword(password) != nil {
-		if c.Request.Method == "OPTIONS" {
-			c.Set("user", guest)
-			c.Next()
-			return
-		}
-		c.Status(http.StatusUnauthorized)
-		c.Abort()
-		return
-	}
-	if user.Disabled || !user.CanWebdavRead() {
-		if c.Request.Method == "OPTIONS" {
-			c.Set("user", guest)
-			c.Next()
-			return
-		}
-		c.Status(http.StatusForbidden)
-		c.Abort()
-		return
-	}
-	if !user.CanWebdavManage() && utils.SliceContains([]string{"PUT", "DELETE", "PROPPATCH", "MKCOL", "COPY", "MOVE"}, c.Request.Method) {
-		if c.Request.Method == "OPTIONS" {
-			c.Set("user", guest)
-			c.Next()
-			return
-		}
-		c.Status(http.StatusForbidden)
-		c.Abort()
-		return
-	}
-	c.Set("user", user)
+	c.Set("user", guest)
 	c.Next()
+	return
 }
